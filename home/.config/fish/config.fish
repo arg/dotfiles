49521cbc678f25a446dd62e56c8b6bbc10d73af1
Andrii Gladkyi <arg@arg.zone>
2021-04-11 15:03:47 +0300

    Gruvboxed default FZF dialog

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -7,6 +7,7 @@ set -gx CLICOLOR "1"
 set -gx TERM "xterm-256color"
 set -gx COLORTERM "truecolor"
 set -gx FZF_DEFAULT_COMMAND "rg --files --follow --hidden"
+set -gx FZF_DEFAULT_OPTS "--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934"
 set -gx MANPAGER "nvim -c 'set ft=man' -"
 
 set -p fish_user_paths "$HOME/.bin"
