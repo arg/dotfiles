b05e909367188674572319e472dac1865d5dff72
Andrii Gladkyi <arg@arg.zone>
2022-04-24 15:30:38 +0200

    Removed unneeded lines from Fish config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -10,10 +10,7 @@ set -gx FZF_DEFAULT_COMMAND "rg --files --follow"
 set -gx FZF_DEFAULT_OPTS "--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934"
 set -gx MANPAGER "nvim +Man!"
 set -gx RUBYOPT "-W0"
-set -gx BUNDLE_PATH "/home/arg/.gem"
-set -gx BUNDLE_BIN "~/.local/bin"
 
-fish_add_path -mP ~/node_modules/.bin
 fish_add_path -mP ~/.local/bin
 
 set fish_greeting ""
