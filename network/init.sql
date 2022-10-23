a02cb584470b843b967c1c66251a89b9af70e597
Andrii Gladkyi <arg@arg.zone>
2022-10-23 21:05:39 +0200

    Updated Docker stack

--- network/init.sql
+++ network/init.sql
@@ -2,4 +2,8 @@ CREATE USER monica IDENTIFIED BY 'monica';
 CREATE DATABASE monica;
 GRANT ALL PRIVILEGES ON monica.* TO 'monica'@'%';
 
+CREATE USER photoprism IDENTIFIED BY 'photoprism';
+CREATE DATABASE photoprism;
+GRANT ALL PRIVILEGES ON photoprism.* TO 'photoprism'@'%';
+
 FLUSH PRIVILEGES;
