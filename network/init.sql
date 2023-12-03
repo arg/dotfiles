d188ec438a6c5b0396c291092b7c3317b545b940
Andrii Gladkyi <arg@arg.zone>
2023-12-03 10:57:21 +0100

    Docker services update

--- network/init.sql
+++ network/init.sql
@@ -2,8 +2,8 @@ CREATE USER monica IDENTIFIED BY 'monica';
 CREATE DATABASE monica;
 GRANT ALL PRIVILEGES ON monica.* TO 'monica'@'%';
 
-CREATE USER photoprism IDENTIFIED BY 'photoprism';
-CREATE DATABASE photoprism;
-GRANT ALL PRIVILEGES ON photoprism.* TO 'photoprism'@'%';
+CREATE USER freshrss IDENTIFIED BY 'freshrss';
+CREATE DATABASE freshrss;
+GRANT ALL PRIVILEGES ON freshrss.* TO 'freshrss'@'%';
 
 FLUSH PRIVILEGES;
