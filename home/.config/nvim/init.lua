b765062ebbbe78bacb1213d29bf8818a3740beed
Andrii Gladkyi <arg@arg.zone>
2024-01-31 18:03:37 +0100

    Removed version pin

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -91,7 +91,6 @@ require("lazy").setup({
   -- nvim-treesitter {{{
   {
     "nvim-treesitter/nvim-treesitter",
-    tag = "v0.9.1",
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
     opts = {
