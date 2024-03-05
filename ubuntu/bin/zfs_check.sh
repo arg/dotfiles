c4fce50db6eb2985762cf2681d94dc0bed575ac1
Andrii Gladkyi <arg@arg.zone>
2024-03-05 07:51:44 +0100

    ZFS health check script

---
+++ ubuntu/bin/zfs_check.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+print_error () {
+  echo $1
+  echo "Status: *******************************************************************************"
+  /sbin/zpool status
+  echo "List: *********************************************************************************"
+  /sbin/zpool list
+  exit 1
+}
+
+pool_statuses=$(/sbin/zpool list -H -o name,health,capacity)
+
+error_statuses=$(grep -v "ONLINE" <<< $pool_statuses)
+if [[ $error_statuses != "" ]]; then
+  print_error "Some pools are in error state"
+fi
+
+capacities=$(awk '{print $3}' <<< $pool_statuses | cut -d'%' -f1)
+for capacity in $capacities; do
+  if [[ $capacity -ge 80 ]]; then
+    print_error "Some pools are approaching their maximum capacity"
+  fi
+done
+
+printf "%s: %s (%s)\n" $pool_statuses
