c52f67da2941a39ee2d140b18869e0628c18c365
Andrii Gladkyi <arg@arg.zone>
2025-04-25 06:03:48 +0200

    Fixed compress macros

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -140,9 +140,9 @@ end
 function compress -a target -a directory -d "Compresses directory"
   switch $target
     case "*.tar"
-      tar -cv --disable-copyfile --no-mac-metadata --no-xattrs --exclude='.DS_Store' -f $target  $directory
+      tar -Cv --disable-copyfile --no-mac-metadata --no-xattrs --exclude='.DS_Store' -f $target $directory
     case "*.tar.gz"
-      tar -czvf $target --no-mac-metadata --no-xattrs $directory
+      tar -Czv --disable-copyfile --no-mac-metadata --no-xattrs --exclude='.DS_Store' -f $target $directory
     case "*.zip" "*.7z"
       7z a $target $directory
     case '*'
